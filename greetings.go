1a9cf0ed36334233ad91a6eb6ff70a82ff08ee8e
brentgroves <brent.groves@gmail.com>
2024-10-28 14:35:10 -0400

    greetings3: added multiple people greeting

--- greetings.go
+++ greetings.go
@@ -22,6 +22,7 @@ func Hello(name string) (string, error) {
 func Hellos(names []string) (map[string]string, error) {
 	// A map to associate names with messages.
 	messages := make(map[string]string)
+
 	// Loop through the received slice of names, calling
 	// the Hello function to get a message for each name.
 	for _, name := range names {
